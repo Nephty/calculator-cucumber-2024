7057eb1d645e0ec3e7ae0175faf6d6385209e179
LicorneRose765 <210376@umons.ac.be>
2024-04-06 16:45:42 +0200

    Abort merge modification on Main. (for Jar)

--- src/main/java/back/calculator/Main.java
+++ src/main/java/back/calculator/Main.java
@@ -1,69 +1,13 @@
 package back.calculator;
 
-import back.logging.LoggerHelper;
-import back.calculator.types.MyNumber;
-import back.calculator.operators.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
- * A very simple calculator in Java
- * University of Mons - UMONS
- * Software Engineering Lab
- * Faculty of Sciences
- *
- * @author tommens
+ *  This class is necessary for the compilation of the .jar file. See issue #16 for more information.
  */
 public class Main {
     public static final Logger logger = Logger.getLogger(Main.class.getName());
-
-    /**
-     * This is the main method of the application.
-     * It provides examples of how to use it to construct and evaluate arithmetic expressions.
-     *
-     * @param args Command-line parameters are not used in this version
-     */
-    public static void main(String[] args) throws IOException {
-        String logFilePath = LoggerHelper.prepareLogger(logger);
-
-        Expression e;
-        Calculator c = new Calculator();
-
-        try {
-            e = new MyNumber(8);
-            c.print(e);
-            c.eval(e);
-
-            List<Expression> params = new ArrayList<>();
-            Collections.addAll(params, new MyNumber(3), new MyNumber(4), new MyNumber(5));
-            e = new Plus(params, Notation.PREFIX);
-            c.printExpressionDetails(e);
-            c.eval(e);
-
-            List<Expression> params2 = new ArrayList<>();
-            Collections.addAll(params2, new MyNumber(5), new MyNumber(3));
-            e = new Minus(params2, Notation.INFIX);
-            c.print(e);
-            c.eval(e);
-
-            List<Expression> params3 = new ArrayList<>();
-            Collections.addAll(params3, new Plus(params), new Minus(params2));
-            e = new Times(params3);
-            c.printExpressionDetails(e);
-            c.eval(e);
-
-            List<Expression> params4 = new ArrayList<>();
-            Collections.addAll(params4, new Plus(params), new Minus(params2), new MyNumber(5));
-            e = new Divides(params4, Notation.POSTFIX);
-            c.print(e);
-            c.eval(e);
-        } catch (IllegalConstruction exception) {
-            System.out.println("cannot create operations without parameters");
-        }
+    public static void main(String[] args) {
+        App.main(args);
     }
-
 }
